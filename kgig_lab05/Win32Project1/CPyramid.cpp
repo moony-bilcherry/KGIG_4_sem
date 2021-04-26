ad17bb1501aa22d49a4cd6ef53567d5ef94f91e7
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-04-26 09:49:31 +0300

    5

--- kgig_lab05/Win32Project1/CPyramid.cpp
+++ kgig_lab05/Win32Project1/CPyramid.cpp
@@ -4,13 +4,12 @@
 
 CPyramid::CPyramid()
 {
-	this->Vertices.RedimMatrix(4, 7);	// создание матрицы с координатами точек пирамиды
-	this->Nabcd.RedimMatrix(3);			// создание матрицы с вектором нормали к ABC
+	this->Vertices.RedimMatrix(4, 6);	// создание матрицы с координатами точек пирамиды
 	//	A	A1	B	B1	C	C1	
-	//	3	1	0	0	0	0	0
-	//	0	0	0	0	3	1	0
-	//	0	3	0	3	0	3	0
-	//	1	1	1	1	1	1	1
+	//	3	1	0	0	0	0
+	//	0	0	0	0	3	1
+	//	0	3	0	3	0	3
+	//	1	1	1	1	1	1
 	this->Vertices(0, 0) = 3;		// A (ось х)
 	this->Vertices(0, 1) = 1;		// A1 (ось х)
 	this->Vertices(2, 1) = 3;		// A1 (ось z)
@@ -18,31 +17,27 @@ CPyramid::CPyramid()
 	this->Vertices(1, 4) = 3;		// C (ось y)
 	this->Vertices(1, 5) = 1;		// C1 (ось у)
 	this->Vertices(2, 5) = 3;		// C1 (ось z)
-	for (int i = 0; i < 7; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		this->Vertices(3, i) = 1;
 	}
-	this->Nabcd(0) = 1;
-	this->Nabcd(1) = 0;
-	this->Nabcd(2) = -1;			// вектор нормали к основанию пирамиды
 }
 
-CMatrix CreateViewCoord(double r, double fi, double q)
+CMatrix CreateViewCoord(double r, double fi, double q)			// ћ—  -> ¬— 
 {
-	// координаты точки наблюдени€
-	double fi_r = (fi / 180) * pi;
-	double q_r = (q / 180) * pi;
+	// сферические координаты точки наблюдени€
+	double fi_sphere = (fi / 180) * pi;		// азимут
+	double q_sphere = (q / 180) * pi;		// зенит
 
-	// заполнение матрицы пересчета из ћ—  в ќ— 
 	CMatrix K(4, 4);
-	K(0, 0) = -sin(fi_r);
-	K(0, 1) = cos(fi_r);
-	K(1, 0) = -cos(q_r) * cos(fi_r);
-	K(1, 1) = -cos(q_r) * sin(fi_r);
-	K(1, 2) = sin(q_r);
-	K(2, 0) = -sin(q_r) * cos(fi_r);
-	K(2, 1) = -sin(q_r) * sin(fi_r);
-	K(2, 2) = -cos(q_r);
+	K(0, 0) = -sin(fi_sphere);
+	K(0, 1) = cos(fi_sphere);
+	K(1, 0) = -cos(q_sphere) * cos(fi_sphere);
+	K(1, 1) = -cos(q_sphere) * sin(fi_sphere);
+	K(1, 2) = sin(q_sphere);
+	K(2, 0) = -sin(q_sphere) * cos(fi_sphere);
+	K(2, 1) = -sin(q_sphere) * sin(fi_sphere);
+	K(2, 2) = -cos(q_sphere);
 	K(2, 3) = r;
 	K(3, 3) = 1;
 	return K;
@@ -51,14 +46,13 @@ CMatrix CreateViewCoord(double r, double fi, double q)
 
 void CPyramid::Draw1(CDC &dc, CMatrix&PView, CRect &RW)			// с удалением ребер
 {
-	CMatrix ViewCart = SphereToCart(PView);		// сферические координаты в декарторы
 	CMatrix XV = CreateViewCoord(PView(0), PView(1), PView(2));		// получаем матрицу пересчета
 	CMatrix ViewVert = XV * this->Vertices;							// Kview * PIR
 	CRectD RectView;
 	GetRect(this->Vertices, RectView);								// задание области
 
 	CMatrix MW = SpaceToWindow(RectView, RW);
-	CPoint MasVert[6], a1b1c1[3], abc[3];
+	CPoint MasVert[6], A1B1C1[3], ABC[3];
 	CMatrix V(3);
 	V(2) = 1;
 	for (int i = 0; i < 6; i++)
@@ -69,12 +63,12 @@ void CPyramid::Draw1(CDC &dc, CMatrix&PView, CRect &RW)			// с удалением ребер
 		MasVert[i].x = (int)V(0);
 		MasVert[i].y = (int)V(1);
 	}
-	abc[0] = MasVert[0];
-	abc[1] = MasVert[2];
-	abc[2] = MasVert[4];
-	a1b1c1[0] = MasVert[1];
-	a1b1c1[1] = MasVert[3];
-	a1b1c1[2] = MasVert[5];
+	ABC[0] = MasVert[0];
+	ABC[1] = MasVert[2];
+	ABC[2] = MasVert[4];
+	A1B1C1[0] = MasVert[1];
+	A1B1C1[1] = MasVert[3];
+	A1B1C1[2] = MasVert[5];
 
 	CPen Pen(PS_SOLID, 2, RGB(0, 0, 0));
 	CPen *pOldPen = dc.SelectObject(&Pen);
@@ -83,64 +77,64 @@ void CPyramid::Draw1(CDC &dc, CMatrix&PView, CRect &RW)			// с удалением ребер
 	CBrush BaseBrush(RGB(251, 217, 255));				// цвет остальных граней
 	CBrush *pOldBrush = dc.SelectObject(&BottopBrush);
 	dc.SelectObject(&BaseBrush);
-	CPoint ab[4];
-	ab[0] = abc[0];
-	ab[1] = abc[1];
-	ab[2] = a1b1c1[1];
-	ab[3] = a1b1c1[0];
+	CPoint ABB1A1[4];
+	ABB1A1[0] = ABC[0];
+	ABB1A1[1] = ABC[1];
+	ABB1A1[2] = A1B1C1[1];
+	ABB1A1[3] = A1B1C1[0];
 
-	CPoint bc[4];
-	bc[0] = abc[1];
-	bc[1] = abc[2];
-	bc[2] = a1b1c1[2];
-	bc[3] = a1b1c1[1];
+	CPoint BCB1C1[4];
+	BCB1C1[0] = ABC[1];
+	BCB1C1[1] = ABC[2];
+	BCB1C1[2] = A1B1C1[2];
+	BCB1C1[3] = A1B1C1[1];
 
-	CPoint ac[4];
-	ac[0] = abc[2];
-	ac[1] = abc[0];
-	ac[2] = a1b1c1[0];
-	ac[3] = a1b1c1[2];
+	CPoint ACC1A1[4];
+	ACC1A1[0] = ABC[2];
+	ACC1A1[1] = ABC[0];
+	ACC1A1[2] = A1B1C1[0];
+	ACC1A1[3] = A1B1C1[2];
 
-	if (PView(1) < 90)				// проверка по углу поворота вокруг оси Z
+	if (PView(1) < 90)				// если азимут < 90
 	{
-		dc.Polygon(ab, 4);
-		dc.Polygon(ac, 4);
+		dc.Polygon(ABB1A1, 4);
+		dc.Polygon(ACC1A1, 4);
 	}
 	else
 	{
-		if (PView(1) < 180)
+		if (PView(1) < 180)			// если азимут 90 < х < 180
 		{
-			dc.Polygon(ac, 4);
-			dc.Polygon(bc, 4);
+			dc.Polygon(ACC1A1, 4);
+			dc.Polygon(BCB1C1, 4);
 		}
 		else
-			if (PView(1) < 270)
+			if (PView(1) < 270)		// если азимут 180 < х < 270
 			{
-				dc.Polygon(ab, 4);
-				dc.Polygon(bc, 4);
+				dc.Polygon(ABB1A1, 4);
+				dc.Polygon(BCB1C1, 4);
 			}
-			else
+			else					// если азимут 270 < х < 360
 			{
-				dc.Polygon(ac, 4);
-				dc.Polygon(ab, 4);
+				dc.Polygon(ACC1A1, 4);
+				dc.Polygon(ABB1A1, 4);
 			}
 	}
 	if (PView(2) <= 10 && PView(1) > 180 && PView(1) < 270)
 	{
-		dc.Polygon(ac, 4);
-		dc.Polygon(ab, 4);
-		dc.Polygon(bc, 4);
+		dc.Polygon(ACC1A1, 4);
+		dc.Polygon(ABB1A1, 4);
+		dc.Polygon(BCB1C1, 4);
 	}
 
-	if (PView(2) <= 90)
+	if (PView(2) <= 90)					// если зенит <= 90
 	{
 		dc.SelectObject(&TopBrush);
-		dc.Polygon(a1b1c1, 3);
+		dc.Polygon(A1B1C1, 3);				// рисуетс€ грань A1B1C1
 	}
-	else
+	else								// если зенит > 90
 	{
 		dc.SelectObject(&BottopBrush);
-		dc.Polygon(abc, 3);
+		dc.Polygon(ABC, 3);					// рисуетс€ грань ABC
 	}
 
 	dc.SelectObject(pOldPen);
@@ -149,7 +143,6 @@ void CPyramid::Draw1(CDC &dc, CMatrix&PView, CRect &RW)			// с удалением ребер
 
 void CPyramid::Draw(CDC &dc, CMatrix&PView, CRect &RW)			// без удалени€ граней
 {
-	CMatrix ViewCart = SphereToCart(PView);
 	CMatrix XV = CreateViewCoord(PView(0), PView(1), PView(2));
 	CMatrix ViewVert = XV * this->Vertices;
 	CRectD RectView;
